4bb797cb2c782f58fb7e1eafb972e00171981047
Jose-gabriel-19 <145858759+Jose-gabriel-19@users.noreply.github.com>
2023-10-07 12:37:11 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,101 @@
+
+cidade = 'guarulhos'
+comprimento  = len(cidade)
+print(comprimento)
+
+semana = 'sexta'
+print(len(semana))
+
+lista = [1,2,3,4]
+print(len(lista))
+
+x = 5.9
+print(type(x))
+
+n = [8,9,0,7,6,5,4,3,2]
+print(sum(n))
+
+print(max(n))
+print(min(n))
+
+z = 44
+n = str(z)
+print(int(z))
+print(n,5)
+
+n = [8,3,5,7,9,0,2,1,4,6]
+print(sorted(n))
+
+cidade = "guarulhos"
+print('\n')
+frag = list(cidade)
+
+
+tr = tuple(frag)
+print(frag)
+
+n = range(1,21)
+print(n)
+
+l = [2,3,4,5]
+t = ['t,e,r,r']
+
+dic = {}
+
+n = ['o','m','a','j']
+print(sorted(n))
+
+for i in range(2,21,):
+  print(i)
+
+for i in range(2,21,2):
+
+
+w = range(5,51,5) 
+print(sum(w))
+
+print('\n')
+
+print(type(n))
+
+print('\n')
+
+no = input('digite seu nome:\n')
+print('olá,',no)
+
+print('\n')
+
+b = range(1,11,2)
+print(sum(b)/5)
+
+palavra = 'python'
+for l in palavra:
+  print(l)
+
+
+lista = ["banana",'uva','cereja']
+for i in lista:
+  print(i,',isso é uma fruta ')
+
+# dicionario
+
+pessoa = {"nome": "João", "idade": 30, "cidade": "São Paulo"}
+for chave, valor in pessoa.items():
+    print(chave, ":", valor)
+
+
+# d = {
+#   "nome" : "ju"
+#   "idade" ;'12'
+#   "endereco" ; 'rua 12'
+# }
+
+# for t1,t2 in d:
+#   print(t1)
+
+m = [[1,2,3], [1,2,3], [1,2,3]]
+
+for i in m:
+  print(i)
+  for n in i:
+    print(n)
